7909702769e31427b79d9c42a41192c07ff4d021
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-01 01:49:26 -0700

    ist method

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -1,5 +1,16 @@
 abstract class Shape{
-    
+    String color;
+    abstract double area();
+    public abstract String toString();
+    public Shape( String color){
+        System.out.println("Shape one is called");
+        this.color= color;
+    }
+    public String getColor(){
+        return color;
+    }
+
+
 
 }
 
