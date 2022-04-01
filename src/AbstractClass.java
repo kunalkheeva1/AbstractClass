1da39a7b46807e548ea1a81869eafdfa3761fd06
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-01 02:00:45 -0700

    ist method

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -1,18 +1,35 @@
-abstract class Shape{
+abstract class Shape {
     String color;
+
     abstract double area();
+
     public abstract String toString();
-    public Shape( String color){
+
+    public Shape(String color) {
         System.out.println("Shape one is called");
-        this.color= color;
+        this.color = color;
     }
-    public String getColor(){
+
+    public String getColor() {
         return color;
     }
-
-
+}
+class Circle extends Shape{
+    double radius;
+    public Circle(String color, double radius){
+        super(color);
+        System.out.println("Circle one is called");
+        this.radius= radius;
+    }
+    double area(){
+        return Math.PI*Math.pow(radius,2);
+    }
+    public String toString(){
+        return "Color of the circle is " + super.color+ " and area is :" + area();
+    }
 
 }
+
 
 
 
