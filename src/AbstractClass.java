f6d1b521666bd994a6436cde35ba14ff6b78c1bb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-02 20:41:39 -0700

    last attempt

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -1,55 +1,46 @@
-abstract class Shape {
-    String color;
+import java.io.*;
 
-
+abstract class Shape{
+    String name;
+    Shape(String name){
+        this.name= name;
+    }
+    public void moveTo(int x, int y){
+        System.out.println(this.name +" has movement from "+ x+ " to "+y);
+    }
     abstract double area();
-    public abstract String toString();
-
-    public Shape(String color) {
-        System.out.println("Shape one is called");
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
-    }
 }
-class Circle extends Shape{
-    double radius;
-    public Circle(String color, double radius){
-        super(color);
-        System.out.println("Circle one is called");
-        this.radius= radius;
-    }
-    double area(){
-        return Math.PI*Math.pow(radius,2);
-    }
-    public String toString(){
-        return "Color of the circle is " + super.getColor()+ " and area is :" + area();
-    }
-}
+
 class Rectangle extends Shape{
-    double length;
-    double breadth;
-    public Rectangle(String color, double length, double breadth){
-        super(color);
+    double length, breadth;
+    Rectangle(double length, double breadth, String name){
+        super(name);
         this.breadth= breadth;
         this.length= length;
-        System.out.println("Rectangle one is called");
+        System.out.println("Area of Rectangle is: " +area());
     }
     double area(){
         return length*breadth;
     }
-    public String toString(){
-        return "Your color is "+ super.getColor() + " and your area is: " + area();
+}
+class Circle extends Shape{
+    double radius;
+    Circle(double radius, String name){
+        super(name);
+        this.radius= radius;
+        System.out.println("Area of circle is: "+ area());
+    }
+    double area(){
+        return Math.PI*Math.pow(radius,2);
     }
 }
-public class AbstractClass {
+public class AbstractClass{
     public static void main(String[] args) {
-    Shape kk = new Circle("Red ",23.43);
-    Shape kn = new Rectangle("Blue", 23,234);
-        System.out.println(kk);
-        System.out.println( kn);
-    }
+        Circle cc = new Circle(2,"Circle");
+        cc.moveTo(2,3);
+        Rectangle rr = new Rectangle(23,34,"Rectangle");
+        rr.moveTo(6,3);
 
+
+    }
 }
